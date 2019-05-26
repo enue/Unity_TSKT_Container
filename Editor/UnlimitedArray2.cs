2ef31bdb1fbdbc054a4e6ea77720fee65943d9f7
enue <nnp@car.ocn.ne.jp>
2019-05-27 07:29:48 +0900

    add UnlimitedArray2

---
+++ Editor/UnlimitedArray2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace TSKT.Tests
+{
+    public class UnlimitedArray2
+    {
+        [Test]
+        public void Test()
+        {
+            var container = new UnlimitedArray2<int>(-1, -1, 10, 10);
+            Assert.True(container.Contains(0, 0));
+            Assert.False(container.Contains(10, 0));
+            container[10, 0] = 42;
+            Assert.True(container.Contains(10, 0));
+            Assert.AreEqual(42, container[10, 0]);
+            container[-2, -5] = 97;
+            Assert.AreEqual(97, container[-2, -5]);
+            Assert.AreEqual(42, container[10, 0]);
+        }
+    }
+}
+
