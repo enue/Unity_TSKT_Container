526b333c48cf1fa5b7c06557d4ff08ebf83cc67e
enue <nnp@car.ocn.ne.jp>
2019-06-09 01:51:33 +0900

    add OrderedMultiDictionary

---
+++ Editor/OrderedMultiDictionary.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace TSKT.Tests
+{
+    public class OrderedMultiDictionary
+    {
+        [Test]
+        public void Test()
+        {
+            var container = new OrderedMultiDictionary<string, float>();
+            container.Add("hoge", 42f);
+            Assert.AreEqual(42f, container["hoge"][0]);
+
+            Assert.AreEqual(0, container["fuga"].Count);
+
+            container.Add("hoge", 97f);
+
+            Assert.False(container.Remove("piyo"));
+            Assert.True(container.Remove("hoge"));
+
+        }
+        [Test]
+        [TestCase(-0.5f, 1f)]
+        [TestCase(-0.5f, 1.5f)]
+        [TestCase(0f, 1f)]
+        public void GetRange(float begin, float end)
+        {
+            var container = new OrderedMultiDictionary<float, string>();
+            container.Add(0f, "hoge");
+            container.Add(0f, "fuga");
+            container.Add(0f, "piyo");
+            container.Add(-1f, "a");
+            container.Add(1f, "b");
+
+            var range = container.GetRange(begin, end).ToArray();
+            Assert.AreEqual(new KeyValuePair<float, string>(0f, "hoge"), range[0]);
+            Assert.AreEqual(new KeyValuePair<float, string>(0f, "fuga"), range[1]);
+            Assert.AreEqual(new KeyValuePair<float, string>(0f, "piyo"), range[2]);
+            Assert.AreEqual(new KeyValuePair<float, string>(1f, "b"), range[3]);
+            Assert.AreEqual(4, range.Length);
+        }
+    }
+}
+
---
+++ Runtime/OrderedMultiDictionary.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+namespace TSKT
+{
+    public class OrderedMultiDictionary<K, V> : IEnumerable<KeyValuePair<K, V>>
+        where K : IComparable<K>
+    {
+        readonly SortedList<K, List<V>> list = new SortedList<K, List<V>>();
+
+        public void Add(K key, V value)
+        {
+            if (!list.TryGetValue(key, out var values))
+            {
+                values = new List<V>();
+                list.Add(key, values);
+            }
+            values.Add(value);
+        }
+
+        public bool TryGetValue(K key, out List<V> result)
+        {
+            return list.TryGetValue(key, out result);
+        }
+
+        public List<V> this[K key]
+        {
+            get
+            {
+                if (!TryGetValue(key, out var result))
+                {
+                    result = new List<V>();
+                    list.Add(key, result);
+                }
+                return result;
+            }
+        }
+
+        public bool Remove(K key)
+        {
+            return list.Remove(key);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public IEnumerable<KeyValuePair<K, V>> GetRange(K begin, K end)
+        {
+            var beginIndex = BinarySearch(begin);
+            if (beginIndex < 0)
+            {
+                beginIndex = ~beginIndex;
+            }
+            var endIndex = BinarySearch(end);
+            if (endIndex < 0)
+            {
+                endIndex = ~endIndex - 1;
+            }
+
+            var count = endIndex - beginIndex + 1;
+            for(int i=0; i<count; ++i)
+            {
+                var key = list.Keys[i + beginIndex];
+                var values = list.Values[i + beginIndex];
+                foreach(var it in values)
+                {
+                    yield return new KeyValuePair<K, V>(key, it);
+                }
+            }
+        }
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        {
+            foreach(var it in list)
+            {
+                foreach(var value in it.Value)
+                {
+                    yield return new KeyValuePair<K, V>(it.Key, value);
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (var it in list)
+            {
+                foreach (var value in it.Value)
+                {
+                    yield return new KeyValuePair<K, V>(it.Key, value);
+                }
+            }
+        }
+
+        int BinarySearch(K k)
+        {
+            var keys = list.Keys;
+ 
+            int low = 0;
+            int high = keys.Count - 1;
+            while (low <= high)
+            {
+                int median = low + ((high - low) >> 1);
+
+                var c = keys[median].CompareTo(k);
+                if (c == 0)
+                {
+                    return median;
+                }
+                if (c < 0)
+                {
+                    low = median + 1;
+                }
+                else
+                {
+                    high = median - 1;
+                }
+            }
+
+            return ~low;
+        }
+    }
+}
