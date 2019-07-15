d7635f957186c60ae2b14f2e473be336f467ecc4
enue <nnp@car.ocn.ne.jp>
2019-07-16 07:36:33 +0900

    0.4.0 ArrayBuilder

---
+++ Editor/ArrayBuilder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Linq;
+
+namespace TSKT.Tests
+{
+    public class ArrayBuilder
+    {
+        [Test]
+        public void Test()
+        {
+            var original = new[] { "hoge", "fuga", "piyo" };
+            var builder = new ArrayBuilder<string>(original.Length);
+            Assert.AreEqual(original.Length, builder.Capacity);
+            builder.Add(original[0]);
+            builder.Add(original[1]);
+            builder.Add(original[2]);
+            Assert.AreEqual(original, builder.Array);
+            Assert.AreEqual(original, builder.ToArray());
+
+            Assert.Catch<System.Exception>(() => builder.Add("foo"));
+        }
+    }
+}
+
---
+++ Runtime/ArrayBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TSKT
+{
+    public struct ArrayBuilder<T> : IEnumerable<T>
+    {
+        readonly T[] array;
+        public T[] Array
+        {
+            get
+            {
+                UnityEngine.Assertions.Assert.AreEqual(array.Length, Length);
+                return array;
+            }
+        }
+
+        public int Capacity => array.Length;
+        public int Length { get; private set; }
+
+        public ArrayBuilder(int count)
+        {
+            array = new T[count];
+            Length = 0;
+        }
+
+        public void Add(T value)
+        {
+            array[Length] = value;
+            ++Length;
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            for (int i = 0; i < Length; ++i)
+            {
+                yield return array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < Length; ++i)
+            {
+                yield return array[i];
+            }
+        }
+    }
+}
+
