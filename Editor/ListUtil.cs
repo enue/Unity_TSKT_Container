30b6d0ce70a98e8a3e170eb3d36ac809c6b2a556
enue <nnp@car.ocn.ne.jp>
2019-09-25 17:36:35 +0900

    0.5.1 ListUtil.RemoveAll

---
+++ Editor/ListUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+namespace TSKT.Tests
+{
+    public class ListUtil
+    {
+        [Test]
+        public void RemoveAll()
+        {
+            var list = new List<int>() { 1, 1, 2, 3, 1, 1, 1 };
+            var removedCount = TSKT.ListUtil.RemoveAll(list, 1);
+            Assert.AreEqual(5, removedCount);
+            Assert.AreEqual(new List<int>() { 2, 3 }, list);
+        }
+    }
+}
---
+++ Runtime/ListUtil.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TSKT
+{
+    public static class ListUtil
+    {
+        public static int RemoveAll<T>(List<T> list, T item)
+        {
+            var removedCount = 0;
+            var index = list.Count - 1;
+            while(index >= 0)
+            {
+                index = list.LastIndexOf(item, index);
+                if (index < 0)
+                {
+                    break;
+                }
+                list.RemoveAt(index);
+                ++removedCount;
+                --index;
+            }
+            return removedCount;
+        }
+    }
+}
