1969992bb86a3ade621549f34a1bde38ddcdf904
enu <otoriden@gmail.com>
2020-11-13 16:12:51 +0900

    0.6.2 SerializableDictionary

---
+++ Assets/Package/Runtime/SerializableDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+namespace TSKT
+{
+    [System.Serializable]
+    public class SerializableDictionary<K, V>
+    {
+        [SerializeField]
+        K[] keys = default;
+
+        [SerializeField]
+        V[] values = default;
+
+        public SerializableDictionary(Dictionary<K, V> source)
+        {
+            keys = source.Keys.ToArray();
+            values = source.Values.ToArray();
+        }
+
+        public Dictionary<K, V> ToDictionary()
+        {
+            var result = new Dictionary<K, V>(keys.Length);
+            for (int i = 0; i < keys.Length; ++i)
+            {
+                result.Add(keys[i], values[i]);
+            }
+            return result;
+        }
+    }
+}
